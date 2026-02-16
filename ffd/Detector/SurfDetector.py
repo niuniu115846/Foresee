8bd78ecdca629fceb6fe773d25d6c2054c64ff7a
Zuo Rui <18262714326@163.com>
2026-02-16 18:28:42 +0800

    Refactor SurfDetector class for consistency

--- ffd/Detector/SurfDetector.py
+++ ffd/Detector/SurfDetector.py
@@ -3,9 +3,7 @@ import cv2
 from Detector.AbstractDetector import AbstractDetector
 
 
-# copy-move forgery detection with surf
 class SurfDetector(AbstractDetector):
-    # Green
     image = None
     key_points = None
     descriptors = None
@@ -17,8 +15,8 @@ class SurfDetector(AbstractDetector):
         self.detectFeature()
         super().__init__(self.image)
 
-    # feature detect(descriptors and keypoints)
     def detectFeature(self):
-        sift = cv2.SIFT_create()  # burayı surf create yap
+        sift = cv2.SIFT_create()
         gray = cv2.cvtColor(self.image, cv2.COLOR_BGR2GRAY)
         self.key_points, self.descriptors = sift.detectAndCompute(gray, None)
+
