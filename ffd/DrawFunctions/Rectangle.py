64aa9047266640858f76764fb55eb32fac0e6b76
Zuo Rui <18262714326@163.com>
2026-02-16 18:31:06 +0800

    Update Rectangle.py

--- ffd/DrawFunctions/Rectangle.py
+++ ffd/DrawFunctions/Rectangle.py
@@ -30,35 +30,7 @@ class DrawRectangle(AbstractShape):
             cv2.rectangle(new_image, (int(k2x[0]) + 10, int(k2n[1]) - 10), (int(k2n[0]) - 10, int(k2x[1]) + 10), self.color, 2)
             cv2.rectangle(new_image, (int(k1x[0]) + 10, int(k1n[1]) - 10), (int(k1n[0]) - 10, int(k1x[1]) + 10), self.color, 2)
             self.image = new_image
-        # elif self.cRectangle == 3:
-        #     point_list, z = np.zeros(len(self.keypoints1)), 0
-        #     z2, z3, z4 = np.array([[0, 0]]), np.array([[0, 0]]), np.array([[0, 0]])
-        #     for k1, k2 in zip(self.keypoints1, self.keypoints2):
-        #         if len(self.keypoints1) > 1:
-        #             p = (k1[0] - k2[0]) / (k1[1] - k2[1])
-        #             point_list[z] = int(p)
-        #             z = z + 1
-        #     for k1, k2 in zip(self.keypoints1, self.keypoints2):
-        #         if len(self.keypoints1) > 1:
-        #             p = (k1[0] - k2[0]) / (k1[1] - k2[1])
-        #             p = int(p)
-        #             if p == max(point_list):
-        #                 newrow = [k1[0], k1[1]]
-        #                 z2 = np.vstack([z2, newrow])
-        #             elif p < 0:
-        #                 newrow = [k1[0], k1[1]]
-        #                 z3 = np.vstack([z3, newrow])
-        #                 newrow = [k2[0], k2[1]]
-        #                 z4 = np.vstack([z4, newrow])
-        #
-        #     k1x, k11x, k2x = np.max(z3, axis=0), np.max(z2, axis=0), np.max(z4, axis=0)
-        #     z2[0], z3[0], z4[0] = k11x, k1x, k2x
-        #     k11n, k1n, k2n = np.min(z2, axis=0), np.min(z3, axis=0), np.min(z4, axis=0)
-        #
-        #     cv2.rectangle(new_image, (int(k2x[0]) + 10, int(k2n[1]) - 10), (int(k2n[0]) - 10, int(k2x[1]) + 10), self.color, 3)
-        #     cv2.rectangle(new_image, (int(k11x[0]) + 10, int(k11n[1]) - 10), (int(k11n[0]) - 10, int(k11x[1]) + 10), self.color, 3)
-        #     cv2.rectangle(new_image, (int(k1x[0]) + 10, int(k1n[1]) - 10), (int(k1n[0]) - 10, int(k1x[1]) + 10), self.color, 3)
-        # self.image = new_image
+       
         elif self.cRectangle == 3:
             egimlist, x = np.empty(0), 0
             reclist1, reclist2, reclist3 = np.empty(shape=[0, 2]), np.empty(shape=[0, 2]), np.empty(shape=[0, 2])
@@ -96,3 +68,4 @@ class DrawRectangle(AbstractShape):
                           self.color, 2)
 
         self.image = new_image
+
