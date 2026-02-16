b867ef7a06ef7d738fc876cf5f7836ca01ccb2f0
Zuo Rui <18262714326@163.com>
2026-02-16 18:28:19 +0800

    Refactor SiftDetector class structure

--- ffd/Detector/SiftDetector.py
+++ ffd/Detector/SiftDetector.py
@@ -2,10 +2,7 @@ import cv2
 
 from Detector.AbstractDetector import AbstractDetector
 
-
-# copy-move forgery detection with sift
 class SiftDetector(AbstractDetector):
-    # blue
     image = None
     key_points = None
     descriptors = None
@@ -17,8 +14,8 @@ class SiftDetector(AbstractDetector):
         self.detectFeature()
         super().__init__(self.image)
 
-    # detect keypoints and descriptors
     def detectFeature(self):
         sift = cv2.SIFT_create()
         gray = cv2.cvtColor(self.image, cv2.COLOR_BGR2GRAY)
         self.key_points, self.descriptors = sift.detectAndCompute(gray, None)
+
